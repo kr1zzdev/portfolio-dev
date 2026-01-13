256930c6fe93c1cdd51ca80642d6ce9882ea6f14
Chrystopher Silvacky <chrystophersilvackyd@gmail.com>
2026-01-12 21:56:54 -0300

    Improve main menu output

--- java-student-management/java-student-management/src/Main.java
+++ java-student-management/java-student-management/src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Student Management System started.");
+        System.out.println("=== Student Management System ===");
+        System.out.println("1 - Add student");
+        System.out.println("2 - List students");
+        System.out.println("3 - Remove student");
+        System.out.println("System ready.");
     }
 
 }
